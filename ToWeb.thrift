815e9d1a26690bbbd1cfb926ab5830148ca3a93a
‘daolin’ <daolin@qq.com>
2017-11-27 17:31:52 +0800

    modify  for kaiji

--- ToWeb.thrift
+++ ToWeb.thrift
@@ -1,5 +1,6 @@
-namespace java Services.ToWeb
+namespace php Services.ToWeb
 service ToWeb
 {
-    string pushData(1:string data);
+    string pushData(1:string data),
+    string insert_pid(1:string type, 2:string workId, 3:string pid)
 }
---
+++ endpointdeamon.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#/usr/bin/endpointdeamon start &
+/usr/bin/python3.4 /home/wangdl/nodedeamon/endpointdeamon.py start &
